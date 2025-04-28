0c49a5dec0d272124d41e774ff511562d5d53632
azzraaa <azrkor26@gmail.com>
2025-04-28 18:44:02 +0200

    Add constructor validation for valid price codes

--- src/Movie.java
+++ src/Movie.java
@@ -2,17 +2,23 @@ public class Movie {
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
+
     private String title;
     private int priceCode;
-    public Movie(String newtitle, int newpriceCode) {
+    
+    public Movie(String newtitle, int newPriceCode) {
         title = newtitle;
-        priceCode = newpriceCode;
+        setPriceCode(newPriceCode);
     }
     public int getPriceCode() {
         return priceCode;
     }
     public void setPriceCode(int arg) {
-        priceCode = arg;
+        if (arg == REGULAR || arg == NEW_RELEASE || arg == CHILDRENS) {
+            priceCode = arg;
+        } else {
+            throw new IllegalArgumentException("Invalid price code");
+        }
     }
     public String getTitle (){
         return title;
